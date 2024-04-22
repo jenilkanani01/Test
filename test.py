fa5f98071a4da0aa69cdc4b76374ae99d7062f70
Jenilkanani8055 <jenilkanani2015@gmail.com>
2024-04-21 19:55:54 -0600

    fix: test file directory change

--- test.py
+++
@@ -1,9 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.DataFrame(dict(
-    x = [1, 3, 2, 4],
-    y = [1, 2, 3, 4]
-))
-fig = px.line(df, x="x", y="y", title="Unsorted Input") 
-fig.show()
